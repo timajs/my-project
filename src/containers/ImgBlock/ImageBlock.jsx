ddd135e69aebf26587eb91e4049a7bfe293d1910
Tima <baimyrsatemirlan@gmail.com>
2022-02-08 11:37:29 +0600

    last commit

--- src/containers/ImgBlock/ImageBlock.jsx
+++ src/containers/ImgBlock/ImageBlock.jsx
@@ -13,7 +13,6 @@ function Image() {
     }
     const randomColor = color ? "black-heart" : "red-heart";
     return(
-        
         <div className='img-block'>
             <FontAwesomeIcon icon={faHeart} className={randomColor} onClick = {changeClick}/>
             <img className="img" onDoubleClick = {changeClick} src="https://images.unsplash.com/photo-1599412181680-9bf26745b358?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"/>
